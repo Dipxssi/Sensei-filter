0e6bb935a64b007ae54c6915ac599fd496978533
Nidhi-2408 <nidhikalburgi23@gmail.com>
2025-02-15 16:20:03 +0530

    updated

--- AI/extension/content.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "blur") {
-        let video = document.querySelector("video");
-        if (video) {
-            video.style.filter = "blur(10px)";
-        }
-    }
-});
